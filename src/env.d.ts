cb46a642c17b9371c46093b913dbc586d5d185d2
Owen Coogan <37589993+OwenCoogan@users.noreply.github.com>
2022-04-27 23:34:14 +0200

    added stuff

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,4 +5,10 @@ declare module '*.vue' {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>
   export default component
+  interface ImportMetaEnv {
+    VITE_API_URL: string,
+  }
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
 }
